83363f1d07289ec6981ff54a03f2adae16cdc9b4
fateme <72307506+fateme-abedini@users.noreply.github.com>
2022-07-30 11:36:41 -0700

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<table style="background-color: green ; border: 1px solid; margin-left: 42% ; margin-top: 150px ; padding: 50px">
+    <tr>
+        <th><h1 style=" text-align: center "> پرداخت شد </h1></th>
+    </tr>
+</table>
+</body>
+</html>
